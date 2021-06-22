1f5f850c21044600f36dd15c24fb747ea449d3f8
g0 <59220768+g0-I@users.noreply.github.com>
2021-06-22 23:56:38 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to create React App
         </a>
       </header>
     </div>
